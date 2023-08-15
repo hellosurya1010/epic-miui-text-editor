cbaa433ccb0d8f321e122beb0f06a659edd92755
hellosurya1010 <ayrus4444@gmail.com>
2023-08-15 20:52:34 +0530

    init

---
+++ src/Editor.tsx
@@ -0,0 +1,49 @@
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+  createTheme,
+  useMediaQuery,
+  type PaletteMode,
+} from "@mui/material";
+import { useCallback, useMemo, useState } from "react";
+import EditorView from "./Editor/Editor";
+
+export default function Editor() {
+  const systemSettingsPrefersDarkMode = useMediaQuery(
+    "(prefers-color-scheme: light)"
+  );
+  const [paletteMode, setPaletteMode] = useState<PaletteMode>(
+    systemSettingsPrefersDarkMode ? "dark" : "light"
+  );
+  const togglePaletteMode = useCallback(
+    () =>
+      setPaletteMode((prevMode) => (prevMode === "light" ? "dark" : "light")),
+    []
+  );
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: paletteMode,
+          secondary: {
+            main: "#42B81A",
+          },
+        },
+      }),
+    [paletteMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <EditorView />
+    </ThemeProvider>
+  );
+}
---
+++ src/Editor/EditorMenuControls.tsx
@@ -0,0 +1,134 @@
+import {
+  MenuButtonAddImage,
+  MenuButtonAddTable,
+  MenuButtonBlockquote,
+  MenuButtonBold,
+  MenuButtonBulletedList,
+  MenuButtonCode,
+  MenuButtonCodeBlock,
+  MenuButtonEditLink,
+  MenuButtonHorizontalRule,
+  MenuButtonIndent,
+  MenuButtonItalic,
+  MenuButtonOrderedList,
+  MenuButtonRedo,
+  MenuButtonRemoveFormatting,
+  MenuButtonStrikethrough,
+  MenuButtonSubscript,
+  MenuButtonSuperscript,
+  MenuButtonTaskList,
+  MenuButtonUnderline,
+  MenuButtonUndo,
+  MenuButtonUnindent,
+  MenuControlsContainer,
+  MenuDivider,
+  MenuSelectFontFamily,
+  MenuSelectFontSize,
+  MenuSelectHeading,
+  MenuSelectTextAlign,
+  isTouchDevice,
+  useRichTextEditorContext,
+} from "mui-tiptap";
+
+export default function EditorMenuControls() {
+  const editor = useRichTextEditorContext();
+  return (
+    <MenuControlsContainer>
+      <MenuSelectFontFamily
+        options={[
+          { label: "Comic Sans", value: "Comic Sans MS, Comic Sans" },
+          { label: "Cursive", value: "cursive" },
+          { label: "Monospace", value: "monospace" },
+          { label: "Serif", value: "serif" },
+        ]}
+      />
+
+      <MenuDivider />
+
+      <MenuSelectHeading />
+
+      <MenuDivider />
+
+      <MenuSelectFontSize />
+
+      <MenuDivider />
+
+      <MenuButtonBold />
+
+      <MenuButtonItalic />
+
+      <MenuButtonUnderline />
+
+      <MenuButtonStrikethrough />
+
+      <MenuButtonSubscript />
+
+      <MenuButtonSuperscript />
+
+      <MenuDivider />
+
+      <MenuButtonEditLink />
+
+      <MenuDivider />
+
+      <MenuSelectTextAlign />
+
+      <MenuDivider />
+
+      <MenuButtonOrderedList />
+
+      <MenuButtonBulletedList />
+
+      <MenuButtonTaskList />
+
+      {/* On touch devices, we'll show indent/unindent buttons, since they're
+      unlikely to have a keyboard that will allow for using Tab/Shift+Tab. These
+      buttons probably aren't necessary for keyboard users and would add extra
+      clutter. */}
+      {isTouchDevice() && (
+        <>
+          <MenuButtonIndent />
+
+          <MenuButtonUnindent />
+        </>
+      )}
+
+      <MenuDivider />
+
+      <MenuButtonBlockquote />
+
+      <MenuDivider />
+
+      <MenuButtonCode />
+
+      <MenuButtonCodeBlock />
+
+      <MenuDivider />
+
+      <MenuButtonAddImage
+        onClick={() => {
+          const url = window.prompt("Image URL");
+
+          if (url) {
+            editor?.chain().focus().setImage({ src: url }).run();
+          }
+        }}
+      />
+
+      <MenuDivider />
+
+      <MenuButtonHorizontalRule />
+
+      <MenuButtonAddTable />
+
+      <MenuDivider />
+
+      <MenuButtonRemoveFormatting />
+
+      <MenuDivider />
+
+      <MenuButtonUndo />
+      <MenuButtonRedo />
+    </MenuControlsContainer>
+  );
+}
